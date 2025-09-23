9ce4a8d2129f5e7480c350d277aea8984bccf49a
alex <berezov86@gmail.com>
2025-09-23 16:12:15 +0300

    add first page

---
+++ hexlet_django_blog/views.py
@@ -0,0 +1,12 @@
+# hexlet_django_blog/views.py
+from django.shortcuts import render
+
+
+def index(request):
+    return render(
+        request,
+        "index.html",
+        context={
+            "who": "World",
+        },
+    )
