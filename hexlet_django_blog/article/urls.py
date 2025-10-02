a2b54e2f9264a11a3b12183def4078f0d89a03eb
volkbav <berezov86@gmail.com>
2025-10-02 20:35:04 +0300

    lesson 8

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from hexlet_django_blog.article.views import ArticleIndexView
 
 urlpatterns = [
-    path("", ArticleIndexView.as_view(), name='index'),
+    path("", ArticleIndexView.as_view(), name='article'),
+    path("<str:tags>/<int:article_id>", ArticleIndexView.as_view(), name='article_id')
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,14 +1,27 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
-
+from django.shortcuts import render, redirect
+#from django.http import HttpResponse
 from django.views import View
-
+from django.urls import reverse
 
 class ArticleIndexView(View):
     template_name = "articles/index.html"
 
     def get(self, request, **kwargs):
-        return render(request, self.template_name , kwargs)
+        if not kwargs:
+            redirect_kwargs = {
+                'tags': 'python',
+                'article_id': 42
+            }
+            return redirect(
+                reverse(
+                    'article_id', 
+                    kwargs=redirect_kwargs
+                )
+            )
+        context={
+            'tags': kwargs.get('tags'),
+            'article_id': kwargs.get('article_id')
+        }
+        return render(request, self.template_name, context)
 
 
