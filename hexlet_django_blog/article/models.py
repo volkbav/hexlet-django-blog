580fca455787f427b591bf289dd6d72a917e6ed2
alex <berezov86@gmail.com>
2025-10-07 16:42:46 +0300

    lesson 9: create first ORM

--- hexlet_django_blog/article/models.py
+++ hexlet_django_blog/article/models.py
@@ -1,2 +1,10 @@
 
 # Create your models here.
+from django.db import models
+
+
+class Article(models.Model):
+    name = models.CharField(max_length=200)  # название статьи
+    body = models.TextField()  # тело статьи
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
