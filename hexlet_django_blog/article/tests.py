547cb03b94c742567af6c428d18c34b31c9c7711
alex <berezov86@gmail.com>
2025-10-13 17:17:18 +0300

    add test for article

--- hexlet_django_blog/article/tests.py
+++ hexlet_django_blog/article/tests.py
@@ -1,2 +1,17 @@
 
 # Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+
+class ArticlesTest(TestCase):
+    def test_articles_list(self):
+        response = self.client.get(reverse("article"))
+        self.assertEqual(response.status_code, 200)
+
+        # Проверяем наличие данных в контексте шаблона
+        self.assertIn("article", response.context)
+        articles = response.context["article"]
+
+        # Проверяем не пустой ли список пользователей
+        self.assertTrue(len(articles) > 0)
