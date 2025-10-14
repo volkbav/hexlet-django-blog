632b2170e1dc84ecaf2297d0908d3951eb589d7b
alex <berezov86@gmail.com>
2025-10-14 15:50:15 +0300

    rename path

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from hexlet_django_blog.article.views import ArticleIndexView, IndexView
 
 urlpatterns = [
-    path("", IndexView.as_view(), name='article'),
+    path("", IndexView.as_view(), name='articles'),
     path('<str:tags>/<int:article_id>', ArticleIndexView.as_view(), name='article_id')
 ]
