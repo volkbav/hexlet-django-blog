37b07e2da4791401617b5195b78cc281d131d041
alex <berezov86@gmail.com>
2025-10-15 14:22:00 +0300

    add view for comments

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -4,6 +4,7 @@ from hexlet_django_blog.article.views import (
     ArticleIndexView, 
     IndexView,
     ArticleView,
+    ArticleCommentFormView
 #    ArticleCommentsView,
 )
 
@@ -12,4 +13,5 @@ urlpatterns = [
     path('<str:tags>/<int:article_id>', ArticleIndexView.as_view(), name='article_id'),
     path("<int:id>/", ArticleView.as_view()),
 #    path('<int:article_id>/comments/<int:id>/', ArticleCommentsView.as_view()),    
+    path('comments/', ArticleCommentFormView.as_view(), name='commetns')
 ]
