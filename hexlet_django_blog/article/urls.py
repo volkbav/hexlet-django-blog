b0b8d19a63fa729fd254d648e0f3a8d8c01fb4d9
volkbav <berezov86@gmail.com>
2025-10-18 14:27:55 +0300

    lesson 19

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -5,7 +5,8 @@ from hexlet_django_blog.article.views import (
     IndexView,
     ArticleView,
     ArticleFormCreateView,
-    ArticleFormEditView
+    ArticleFormEditView,
+    ArticleFormDeleteView,
 )
 
 urlpatterns = [
@@ -13,5 +14,6 @@ urlpatterns = [
     path('<str:tags>/<int:article_id>', ArticleIndexView.as_view(), name='article_id'),
     path("<int:id>/update/", ArticleFormEditView.as_view(), name="articles_update"),
     path("<int:id>/", ArticleView.as_view()),
+    path("<int:id>/delete/", ArticleFormDeleteView.as_view(), name="articles_delete"),
     path("create/", ArticleFormCreateView.as_view(), name="articles_create"),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -50,6 +50,7 @@ class ArticleFormCreateView(View):
         # Если данные некорректные, то возвращаем человека обратно на страницу с заполненной формой
         return render(request, 'articles/create.html', {'form': form})
 
+
 class ArticleFormEditView(View):
     def get(self, request, *args, **kwargs):
         article_id = kwargs.get("id")
@@ -72,6 +73,15 @@ class ArticleFormEditView(View):
             request, "articles/update.html", {"form": form, "article_id": article_id}
         )
 
+
+class ArticleFormDeleteView(View):
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get("id")
+        article = Article.objects.get(id=article_id)
+        if article:
+            article.delete()
+        return redirect("articles")
+    
 # --- ниже код не используется ---
 # Модель Comments не реализована, поэтому закомментировано
 #class ArticleCommentsView(View):
